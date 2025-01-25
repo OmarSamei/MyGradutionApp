3f89c4133d5ebe3b35f81bd7dc860ae27342cfa2
Omar Sami <omarsamy10000@gmail.com>
2025-01-25 16:10:39 +0200

    Initial commit or Update project

--- graduation_project/lib/Screens/AppContent/home_page.dart
+++ graduation_project/lib/Screens/AppContent/home_page.dart
@@ -2,18 +2,40 @@ import 'package:flutter/material.dart';
 import 'package:graduation_project/Screens/AppContent/Diagnosis_page.dart';
 
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 3; // الصفحة الحالية (افتراضيًا الصفحة الرئيسية)
+
+  // تعريف الصفحات لكل زر
+  final List<Widget> _pages = [
+    SettingsPage(),  // صفحة الإعدادات
+    MenuPage(),      // صفحة القائمة
+    ChatBotPage(),   // صفحة شات بوت
+    const HomeContent(), // الصفحة الرئيسية
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(preferredSize: const Size.fromHeight(60),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(60),
         child: AppBar(
-          
           backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-          elevation: 0, // إزالة الظل
-          leading: IconButton(iconSize: 40,
+          elevation: 0,
+          leading: IconButton(
+            iconSize: 40,
             icon: const Icon(Icons.notifications_none_outlined, color: Colors.black),
             onPressed: () {}, // الإجراء عند الضغط
           ),
@@ -23,10 +45,10 @@ class HomePage extends StatelessWidget {
               const Text(
                 "!..مرحبا بيك ",
                 style: TextStyle(
-                fontFamily: "Alexandria",
+                  fontFamily: "Alexandria",
                   color: Colors.blue,
-                            fontSize: 26,
-                            fontWeight: FontWeight.w700,
+                  fontSize: 26,
+                  fontWeight: FontWeight.w700,
                 ),
               ),
               const SizedBox(width: 10),
@@ -39,87 +61,136 @@ class HomePage extends StatelessWidget {
         ),
       ),
       backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) {
-                          return DiagnosisPage();
-                        }));
-                },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.grey[200],
-                padding: const EdgeInsets.symmetric(horizontal: 63, vertical: 27),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  side: BorderSide(color: Colors.grey.shade400),
-                ),
-              ),
-              child: const Text(
-                'تشخيص',
-                style: TextStyle(
-                  fontFamily: "Alexandria",
-                          color: Colors.blue,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w700,
-                  ),
-              ),
-            ),
-            const SizedBox(height: 50),
-            ElevatedButton(
-              onPressed: () {
-                // الإجراء عند الضغط على الزر الثاني
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.grey[200],
-                padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 28),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  side: BorderSide(color: Colors.grey.shade400),
-                ),
-              ),
-              child: const Text(
-                'دعم ونصائح',
-                style: TextStyle(
-                fontFamily: "Alexandria",
-                          color: Colors.blue,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w700,
-                  ),
-              ),
-            ),
-          ],
-        ),
+      body: IndexedStack(
+        index: _currentIndex,
+        children: _pages,
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.person_outline),
-            label: '',
+            label: 'Settings',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.list_outlined),
-            label: '',
+            label: 'Menu',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.message),
-            label: '',
+            label: 'ChatBot',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.home_outlined),
-            label: '',
+            label: 'Home',
           ),
         ],
+        currentIndex: _currentIndex,
         iconSize: 39,
         selectedItemColor: Colors.blue,
         unselectedItemColor: Colors.black54,
         showSelectedLabels: false,
         showUnselectedLabels: false,
-        currentIndex: 3,
-        
+        onTap: _onItemTapped, // التبديل بين الصفحات
       ),
+    );
+  }
+}
+
+// صفحة المحتوى الرئيسية
+class HomeContent extends StatelessWidget {
+  const HomeContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) {
+                return DiagnosisPage();
+              }));
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.grey[200],
+              padding: const EdgeInsets.symmetric(horizontal: 63, vertical: 27),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+                side: BorderSide(color: Colors.grey.shade400),
+              ),
+            ),
+            child: const Text(
+              'تشخيص',
+              style: TextStyle(
+                fontFamily: "Alexandria",
+                color: Colors.blue,
+                fontSize: 16,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ),
+          const SizedBox(height: 50),
+          ElevatedButton(
+            onPressed: () {
+              // الإجراء عند الضغط على الزر الثاني
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.grey[200],
+              padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 28),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+                side: BorderSide(color: Colors.grey.shade400),
+              ),
+            ),
+            child: const Text(
+              'دعم ونصائح',
+              style: TextStyle(
+                fontFamily: "Alexandria",
+                color: Colors.blue,
+                fontSize: 16,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// صفحة الإعدادات
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Settings Page', style: TextStyle(fontSize: 24)),
+    );
+  }
+}
+
+// صفحة القائمة
+class MenuPage extends StatelessWidget {
+  const MenuPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Menu Page', style: TextStyle(fontSize: 24)),
+    );
+  }
+}
+
+// صفحة شات بوت
+class ChatBotPage extends StatelessWidget {
+  const ChatBotPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('ChatBot Page', style: TextStyle(fontSize: 24)),
     );
   }
 }
--- graduation_project/lib/Screens/Questions_pages/main_questions_page.dart
+++ graduation_project/lib/Screens/Questions_pages/main_questions_page.dart
@@ -11,30 +11,45 @@ class QuestionsPage extends StatelessWidget {
         backgroundColor: Colors.white,
         elevation: 0,
         leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Colors.black),
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
           onPressed: () {
             Navigator.pop(context);
           },
         ),
       ),
       body: ListView(
-        padding: EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(16.0),
         children: [
-          Text(
-            'الأسئلة',
-            style: TextStyle(
-              fontSize: 24.0, // حجم الخط الكبير
-              fontWeight: FontWeight.bold, // جعل الخط عريض
-              color: Colors.black, // لون الخط أسود
-            ),
-            textAlign: TextAlign.center, // جعل النص في المنتصف
-          ),
-          SizedBox(height: 30), // مسافة بين النص والقائمة
-          _buildListTile(context, 'السلوكيات التكرارية أو المقيدة', RepetitiveBehaviorsPage()),
-          _buildListTile(context, 'التفاعل الاجتماعي', SocialInteractionPage()),
-          _buildListTile(context, 'التواصل اللفظي وغير لفظي', CommunicationPage()),
-          _buildListTile(context, 'العناية بالذات', SelfCarePage()),
-          _buildListTile(context, 'الانتباه', AttentionPage()),
+            Image.asset("Photos/gradLogo1.png",
+              width: 100,
+              height: 150),
+   Container(
+  decoration: BoxDecoration( // لون الخلفية اللبني
+    border: Border.all(
+      color: Colors.blue, // لون الحواف الزرقاء
+      width: 2.0, // سمك الحواف
+    ),
+    borderRadius: BorderRadius.circular(8.0), // جعل الحواف مستديرة إذا كنت تريد
+  ),
+  padding: const EdgeInsets.all(8.0), // مسافة بين النص والحواف
+  child: const Text(
+    'الأسئلة',
+    style: TextStyle(
+      fontFamily: "Alexandria",
+      fontSize: 24.0, // حجم الخط الكبير
+      fontWeight: FontWeight.bold, // جعل الخط عريض
+      color: Colors.blue, // لون الخط أسود
+    ),
+    textAlign: TextAlign.center, // جعل النص في المنتصف
+  ),
+),
+
+          const SizedBox(height: 80), // مسافة بين النص والقائمة
+          _buildListTile(context, 'السلوكيات التكرارية أو المقيدة' , const RepetitiveBehaviorsPage()),
+          _buildListTile(context, 'التفاعل الاجتماعي', const SocialInteractionPage()),
+          _buildListTile(context, 'التواصل اللفظي وغير لفظي', const CommunicationPage()),
+          _buildListTile(context, 'العناية بالذات', const SelfCarePage()),
+          _buildListTile(context, 'الانتباه', const AttentionPage()),
         ],
       ),
     );
@@ -44,9 +59,9 @@ class QuestionsPage extends StatelessWidget {
     return ListTile(
       title: Text(
         title,
-        style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+        style: const TextStyle(fontFamily: "Alexandria",fontSize: 18.0, fontWeight: FontWeight.bold),
       ),
-      trailing: Icon(Icons.arrow_forward_ios, color: Colors.black), // سهم باتجاه اليمين
+      trailing: const Icon(Icons.arrow_forward_ios, color: Colors.black), // سهم باتجاه اليمين
       onTap: () {
         Navigator.push(
           context,
@@ -59,61 +74,75 @@ class QuestionsPage extends StatelessWidget {
 
 // Placeholder pages
 class RepetitiveBehaviorsPage extends StatelessWidget {
+  const RepetitiveBehaviorsPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('السلوكيات التكرارية أو المقيدة'),
+        title: const Text('السلوكيات التكرارية أو المقيدة',
+        style: TextStyle(
+            fontFamily: "Alexandria",
+            color: Colors.black
+        ),),
       ),
-      body: Center(child: Text('صفحة السلوكيات التكرارية أو المقيدة')),
+      body: const Center(child: Text('صفحة السلوكيات التكرارية أو المقيدة')),
     );
   }
 }
 
 class SocialInteractionPage extends StatelessWidget {
+  const SocialInteractionPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('التفاعل الاجتماعي'),
+        title: const Text('التفاعل الاجتماعي'),
       ),
-      body: Center(child: Text('صفحة التفاعل الاجتماعي')),
+      body: const Center(child: Text('صفحة التفاعل الاجتماعي')),
     );
   }
 }
 
 class CommunicationPage extends StatelessWidget {
+  const CommunicationPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('التواصل اللفظي وغير لفظي'),
+        title: const Text('التواصل اللفظي وغير لفظي'),
       ),
-      body: Center(child: Text('صفحة التواصل اللفظي وغير لفظي')),
+      body: const Center(child: Text('صفحة التواصل اللفظي وغير لفظي')),
     );
   }
 }
 
 class SelfCarePage extends StatelessWidget {
+  const SelfCarePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('العناية بالذات'),
+        title: const Text('العناية بالذات'),
       ),
-      body: Center(child: Text('صفحة العناية بالذات')),
+      body: const Center(child: Text('صفحة العناية بالذات')),
     );
   }
 }
 
 class AttentionPage extends StatelessWidget {
+  const AttentionPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('الانتباه'),
+        title: const Text('الانتباه'),
       ),
-      body: Center(child: Text('صفحة الانتباه')),
+      body: const Center(child: Text('صفحة الانتباه')),
     );
   }
 }
